52fe6ef9e7985f334b77fe684d589ef528583882
Moises Perez <pr20028@.ues.edu.sv>
2022-11-03 09:36:10 -0600

    Se agrega reactividad

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,35 @@
 <template>
-  <q-page class="flex flex-center">
-
+  <q-page>
+    <div class="row">
+      <div class="col-2 bg-positive">
+        <div>
+          <q-toggle v-model="value" />
+        </div>
+        <div class="q-pa-md">
+          <q-option-group :options="options" type="checkbox" v-model="group" />
+        </div>
+      </div>
+      <div class="col bg-negative">2</div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+
+  setup () {
+    return {
+      value: ref(true),
+      group: ref([]),
+      options: [
+        { label: 'Battery too low', value: 'bat' },
+        { label: 'Friend request', value: 'friend', color: 'green' },
+        { label: 'Picture uploaded', value: 'upload', color: 'red' }
+      ]
+    }
+  }
 })
 </script>
