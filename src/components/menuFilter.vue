e4f69ed459419aa04ff581a3bcfb82977b987b99
Mariana Santos <gs20027@edu.ues.sv>
2022-11-05 12:06:17 -0400

    menuFilter en component

---
+++ src/components/menuFilter.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <q-toggle v-model="value" label="Nuevo"/>
+        </div>
+            <div class="q-pa-md" >
+        <q-option-group :options="options" type="checkbox" v-model="group" />
+        </div>
+</template>
+<script>
+import { ref } from 'vue'
+
+export default ({
+  setup () {
+    return {
+      value: ref(false),
+      group: ref([]),
+      options: [
+        { label: 'Samsung', value: 'samsung' },
+        { label: 'Huawei', value: 'huawei' },
+        { label: 'Nokia', value: 'nokia' },
+        { label: 'Iphone', value: 'iphone' },
+        { label: 'Xiaomi', value: 'xiami' }
+
+      ]
+    }
+  }
+})
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,67 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page>
+    <div class="row">
+      <div class="col-2 bg-negative">
+        <MenuFilter />
+      </div>
+      <div class="col">
+        <div class="row">
+          <div class="col q-ma-lg">
+            <fieldset>
+              <legend> </legend>
+              <div class="row">
+                <div class="col-5 q-ma-sm">
+                  <q-input standout v-model="precio" type="number" min="0" prefix="Precio">
+                    <template v-slot:append>
+                      <q-icon name="paid" />
+                    </template>
+                  </q-input>
+                </div>
+                <div class="col-5 q-ma-sm">
+                  <q-input standout v-model="hasta" type="number" min="0" prefix="Hasta">
+                    <template v-slot:append>
+                      <q-icon name="paid" />
+                    </template>
+                  </q-input>
+                </div>
+              </div>
+            </fieldset>
+          </div>
+          <div class="col q-ma-lg">
+            <fieldset>
+              <legend> </legend>
+              <div class="row">
+                <div class="col-5 q-ma-sm">
+                  <q-btn square color="primary" icon="north" v-model="ordenPrecio" label="precio"/>
+                </div>
+              </div>
+              <div class="col-5 q-ma-sm">
+                  <q-btn square color="primary"  v-model="fecha" label="fecha" />
+                </div>
+            </fieldset>
+          </div>
 
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import MenuFilter from 'src/components/menuFilter.vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: { MenuFilter },
+  setup () {
+    return {
+      precio: ref(''),
+      hasta: ref(''),
+      ordenPrecio: ref('ordenPrecio'),
+      fecha: ref('fecha')
+
+    }
+  }
 })
 </script>
