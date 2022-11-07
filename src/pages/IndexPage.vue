9fd0e66665d43c67f52a401059bfd664548617fa
Moises Perez <pr20028@.ues.edu.sv>
2022-11-06 22:35:36 -0600

    Visualizacion solo para PC

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,55 +1,57 @@
 <template>
   <q-page>
-    <div class="row">
-      <div class="col-2.5">
-        <q-scroll-area style="height: 500px; width: 200px;">
-          <MenuFilter />
-        </q-scroll-area>
-      </div>
-      <div class="col">
-        <div class="row">
-          <div class="col q-ma-lg ">
-            <fieldset>
-              <legend>&nbsp;</legend>
-              <div class="row">
-                <div class="col-5 q-mr-sm ">
-                  <q-input standout v-model="precio" type="number" min="0" prefix="Precio">
-                    <template v-slot:append>
-                      <q-icon name="paid" />
-                    </template>
-                  </q-input>
+    <div class="gt-sm ">
+      <div class="row">
+        <div class="col-2.5">
+          <q-scroll-area style="height: 500px; width: 200px;">
+            <MenuFilter />
+          </q-scroll-area>
+        </div>
+        <div class="col">
+          <div class="row">
+            <div class="col q-ma-lg ">
+              <fieldset>
+                <legend>&nbsp;</legend>
+                <div class="row">
+                  <div class="col-5 q-mr-sm ">
+                    <q-input standout v-model="precio" type="number" min="0" prefix="Precio">
+                      <template v-slot:append>
+                        <q-icon name="paid" />
+                      </template>
+                    </q-input>
+                  </div>
+                  <div class="col-5 q-mr-sm ">
+                    <q-input standout v-model="hasta" type="number" min="0" prefix="Hasta">
+                      <template v-slot:append>
+                        <q-icon name="paid" />
+                      </template>
+                    </q-input>
+                  </div>
                 </div>
-                <div class="col-5 q-mr-sm ">
-                  <q-input standout v-model="hasta" type="number" min="0" prefix="Hasta">
-                    <template v-slot:append>
-                      <q-icon name="paid" />
-                    </template>
-                  </q-input>
+              </fieldset>
+            </div>
+            <div class="col q-ma-lg">
+              <fieldset>
+                <legend>&nbsp;</legend>
+                <div class="row">
+                  <div class="col-3 q-mt-md">
+                    <spam>ordenado por</spam>
+                  </div>
+                  <div class="col-3 q-ma-sm">
+                    <q-btn-toggle v-model="ordenarPor" spread icon="north" no-caps toggle-color="purple" color="white"
+                      text-color="black" :options="[
+                        { label: 'Precio', value: 'precio' }
+                      ]" />
+                  </div>
+                  <div class="col-3 q-ma-sm">
+                    <q-btn-toggle v-model="ordenarPor" spread no-caps toggle-color="purple" color="white"
+                      text-color="black" :options="[
+                        { label: 'Fecha', value: 'fecha' }
+                      ]" />
+                  </div>
                 </div>
-              </div>
-            </fieldset>
-          </div>
-          <div class="col q-ma-lg">
-            <fieldset>
-              <legend>&nbsp;</legend>
-              <div class="row">
-                <div class="col-3 q-mt-md">
-                  <spam>ordenado por</spam>
-                </div>
-                <div class="col-3 q-ma-sm">
-                  <q-btn-toggle v-model="ordenarPor" spread icon="north" no-caps toggle-color="purple" color="white"
-                    text-color="black" :options="[
-                      { label: 'Precio', value: 'precio' }
-                    ]" />
-                </div>
-                <div class="col-3 q-ma-sm">
-                  <q-btn-toggle v-model="ordenarPor" spread no-caps toggle-color="purple" color="white"
-                    text-color="black" :options="[
-                      { label: 'Fecha', value: 'fecha' }
-                    ]" />
-                </div>
-              </div>
-            </fieldset>
+              </fieldset>
+            </div>
           </div>
         </div>
       </div>
