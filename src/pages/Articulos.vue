e8fd13f373b50b4794097c1a7e51f1fbc0f596ae
Mariana Santos <gs20027@edu.ues.sv>
2022-11-10 23:10:00 -0300

    'movil_Articulos'

--- src/pages/Articulos.vue
+++ src/pages/Articulos.vue
@@ -1,45 +1,118 @@
 <template>
+  <div class="gt-sm ">
   <div class="row">
     <div class="col bg-purple">
       <carruselArticulos />
     </div>
-    <div class="col bg-green">
+    <div class="col">
+      <!--primer fila-->
+      <div class="row q-ma-lg">
+      <span>Iphone 6 pantalla de 8 pulgadas, 64Gb internos,
+2Gb de Ram, Sólo Banda Tigo, Nuevo
+</span>
+<div class="row q-mx-lg"></div>
+<h3>$235.00</h3>
+</div>
       <div class="row">
-        <!--primer fila-->
-        <span>6</span>
-        <div class="row">
-          <div class="col-10">
-            <q-btn-toggle v-model="comprar" spread no-caps toggle-color="purple" color="white" text-color="black"
-              :options="[
-                { label: 'comprar', value: 'comprar' }
-              ]" />
-            <div class="row">
-              <div class="col">
-                <fieldset>
-                  <legend>&nbsp;</legend>
-                  <div class="row">
-                    <div class="col">
-                      <spam>vendedor:</spam>
-                    </div>
-                    <div class="col">
-                      <spam>telefono:</spam>
-                    </div>
+        <div class="col-5 q-ma-lg ">
+          <q-btn-toggle v-model="comprar" spread no-caps toggle-color="purple" color="white" text-color="black"
+            :options="[
+              { label: 'comprar', value: 'comprar' }
+            ]" />
+          <div class="row q-ma-lg">
+            <div class="col">
+              <fieldset>
+                <legend>&nbsp;</legend>
+                <div class="row">
+                  <div class="col">
+                    <spam>vendedor:</spam>
                   </div>
-                </fieldset>
-              </div>
-
+                  <div class="col">
+                    <spam>telefono:</spam>
+                  </div>
+                </div>
+              </fieldset>
             </div>
           </div>
         </div>
-
       </div>
     </div>
-    <div class="row">
-      <div class="col">56</div>
+  </div>
+  <div class="row">
+        <div class="col text-center">
+          <h4>DESCRIPCION</h4>
+          <div class="row">
+            <div class="col">
+              <textarea rows="8" cols="50">Estado: Nuevo
+Marca: Iphone
+Modelo: 6 plus
+Pantalla: 7 pulgadas
+Sistema: Ios
+Rom: 64GB
+Ram: 2GB</textarea>
+            </div>
+            <div class="col">
+              <textarea rows="8" cols="50">Telefono en muy buenas condiciones, tiene
+dos cámaras, la caja esta abierta, pero con
+muy poco uso, practicamente nuevo, la
+compañia con la que estaba es Tigo, asi que el
+chip debe ser tigo, viene con su caja original y
+con todos sus accesorios. Puedo mandar más
+fotos si desea por whatsapp</textarea>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+      <!--Solo para moviles-->
+      <div class="lt-md">
+        <div class="row">
+          <div class="col">
+            <carruselArticulos></carruselArticulos>
+          </div>
+        </div>
+        <div class="row q-ma-lg">
+      <span>Iphone 6 pantalla de 8 pulgadas, 64Gb internos,
+2Gb de Ram, Sólo Banda Tigo, Nuevo
+</span>
+<div class="row q-mx-lg ">
+  <div class="text-center">
+<h3>$235.00</h3>
+</div>
+</div>
+</div>
+<div class="row">
+  <div class="col-5  q-ma-xs text-center">
+<h6>vendedor:</h6>
+  </div>
+  <div class="col-3  q-ma-xs text-center">
+    <h6>telefono:</h6>
+  </div>
+  </div>
+  <div class="row">
+    <div class="col ">
+      <textarea rows="8" cols="50">Marca: Iphone
+Modelo: 6 plus
+Pantalla: 7 pulgadas
+Sistema: Ios
+Rom: 64GB
+Ram: 2GB</textarea>
     </div>
   </div>
-</template>
+  <div class="row">
+    <div class="col ">
+      <textarea rows="8" cols="50">Telefono en muy buenas condiciones, tiene
+dos cámaras, la caja esta abierta, pero con
+muy poco uso, practicamente nuevo, la
+compañia con la que estaba es Tigo, asi que el
+chip debe ser tigo, viene con su caja original y
+con todos sus accesorios. Puedo mandar más
+fotos si desea por whatsapp</textarea>
+    </div>
+  </div>
+      </div>
 
+</template>
 <script>
 import { defineComponent, ref } from 'vue'
 import carruselArticulos from 'src/components/carruselArticulos.vue'
