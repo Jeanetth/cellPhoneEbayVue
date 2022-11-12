7529ce2042249fc6f891b288247054b346bbfe03
Moises Perez <pr20028@.ues.edu.sv>
2022-11-11 23:46:01 -0600

    50% NUEVO ANUNCIO

---
+++ src/components/adDos.vue
@@ -0,0 +1,66 @@
+<template>
+  <fieldset>
+    <legend>Imágenes</legend>
+    <div class="row">
+      <div class="col text-center">
+        <input id="fileUpload" type="file" hidden />
+        <q-btn round size="25px" color="purple" icon="add" class="q-my-lg" @click="chooseFiles()" />
+        <br>
+        <q-btn round size="25px" color="negative" icon="remove" class="q-my-lg" />
+      </div>
+      <div class="col ">
+        <q-table :rows="rows" :columns="columns" row-key="name" hide-pagination />
+      </div>
+      <div class="col">
+        <q-img class="q-ml-lg" :src="url" spinner-color="white" style="height: 200px; max-width: 200px"
+          @click="refresh" />
+      </div>
+    </div>
+  </fieldset>
+</template>
+<script>
+import { defineComponent, ref } from 'vue'
+const columns = [
+  { name: 'N', align: 'center', label: 'N', field: 'N', sortable: true },
+  { name: 'Tamaño', align: 'center', label: 'Tamaño', field: 'Tamaño', sortable: true },
+  { name: 'Tipo', align: 'center', label: 'Tipo', field: 'Tipo', sortable: true }
+]
+const rows = [
+  {
+    N: 1,
+    Tamaño: '145kb',
+    Tipo: 'jpg'
+  },
+  {
+    N: 2,
+    Tamaño: '145kb',
+    Tipo: 'jpg'
+  },
+  {
+    N: 3,
+    Tamaño: '145kb',
+    Tipo: 'jpg'
+  },
+  {
+    N: 4,
+    Tamaño: '145kb',
+    Tipo: 'jpg'
+  }
+]
+export default defineComponent({
+
+  setup () {
+    const url = ref('https://placeimg.com/500/300/nature')
+
+    return {
+      columns,
+      rows,
+      url,
+      refresh () {
+        url.value = 'https://placeimg.com/500/300/nature?t=' + Math.random()
+      }
+    }
+  }
+})
+
+</script>
--- src/pages/NuevoAnuncio.vue
+++ src/pages/NuevoAnuncio.vue
@@ -1,58 +1,40 @@
 <template>
   <q-page>
-    <div class="row flex-center">
-      <h3>NUEVO ANUNCIO</h3>
-    </div>
-    <div class="row">
-      <div class="col q-ml-lg">
-        <div class="row">
-          <div class="col-11">
-            <fieldset>
-              <legend>&nbsp;</legend>
-              <spam>Estado:</spam>
-              <q-radio v-model="shape" val="Nuevo" label="Nuevo" />
-              <q-radio v-model="shape" val="Usado" label="Usado" />
-              <br>
-              <q-input class="q-mt-lg" outlined v-model="text" :dense="dense">
-                <template v-slot:prepend>
-                  <spam>Marca:</spam>
-                </template>
-              </q-input>
-              <q-input class="q-my-lg" outlined v-model="text" :dense="dense">
-                <template v-slot:prepend>
-                  <spam>Modelo:</spam>
-                </template>
-              </q-input>
-              <q-input outlined v-model="text" :dense="dense">
-                <template v-slot:prepend>
-                  <spam>Pantalla:</spam>
-                </template>
-              </q-input>
-              <div class="row">
-                <div class="col">
-                  <spam>Ordenar por:</spam>
-                </div>
-                <div class="col-5">
-                  <q-select v-model="ordenPor" :options="optionsMobile"/>
-                </div>
-              </div>
-            </fieldset>
+    <div class="gt-sm ">
+      <div class="row flex-center">
+        <h3>NUEVO ANUNCIO</h3>
+      </div>
+      <div class="row">
+        <div class="col q-ml-lg">
+          <div class="row">
+            <div class="col-10">
+              <adUno/>
+            </div>
+          </div>
+          <div class="row q-my-lg">
+            <div class="col-11">
+              <adDos/>
+            </div>
           </div>
         </div>
+        <div class="col bg-negative">2</div>
       </div>
-      <div class="col bg-negative">2</div>
     </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
+import { defineComponent } from 'vue'
+import adUno from 'src/components/adUno.vue'
+import adDos from 'src/components/adDos.vue'
+
 export default defineComponent({
   name: 'nuevoAnuncio',
-  setup () {
-    return {
-      shape: ref('line')
+  methods: {
+    chooseFiles: function () {
+      document.getElementById('fileUpload').click()
     }
-  }
+  },
+  components: { adUno, adDos }
 })
 </script>
