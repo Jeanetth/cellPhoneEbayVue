5f9c442de47c4c6605c7e93105856bd65f892339
Moises Perez <pr20028@.ues.edu.sv>
2022-11-12 17:40:11 -0600

    Se termina Nuevo Anuncio para PC

--- src/pages/NuevoAnuncio.vue
+++ src/pages/NuevoAnuncio.vue
@@ -1,6 +1,6 @@
 <template>
-  <q-page>
-    <div class="gt-sm bg-purple-3">
+  <q-page class="bg-purple-3">
+    <div class="gt-sm">
       <div class="row flex-center">
         <h3>NUEVO ANUNCIO</h3>
       </div>
@@ -17,25 +17,13 @@
             </div>
           </div>
         </div>
-        <div class="col bg-negative">
-          <div class="row">
-            <div class="col-12 bg-positive">
-             2
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-12 bg-positive">
-             2
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-12 bg-positive">
-             2
-            </div>
-          </div>
-
+        <div class="col ">
+          <adTres/>
         </div>
       </div>
+    </div>
+    <div class="lt-md">
+      <!--Solo para moviles-->
     </div>
   </q-page>
 </template>
@@ -44,9 +32,10 @@
 import { defineComponent } from 'vue'
 import adUno from 'src/components/adUno.vue'
 import adDos from 'src/components/adDos.vue'
+import adTres from 'src/components/adTres.vue'
 
 export default defineComponent({
   name: 'nuevoAnuncio',
-  components: { adUno, adDos }
+  components: { adUno, adDos, adTres }
 })
 </script>
